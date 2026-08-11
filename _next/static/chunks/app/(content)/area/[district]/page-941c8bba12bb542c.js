2665dfe6b64f45eff2659f78407851a012925a30
TheDesigner56 <35447879+TheDesigner56@users.noreply.github.com>
2026-08-11 22:18:57 +0100

    Deploy 2026-08-11T21:18:43Z: Atlas redesign + .nojekyll fix (Jekyll was hiding _next/)

---
+++ _next/static/chunks/app/(content)/area/[district]/page-941c8bba12bb542c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[555,312],{4577:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=4577)}),_N_E=n.O()}]);
---
+++ _next/static/lU_eOhFxQ7gDqSw8UR8Dw/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Farea\u002F[district]","\u002Fschool\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
